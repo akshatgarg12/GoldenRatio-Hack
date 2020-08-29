af048fb960bc7bfa097aa0cdb5725a1402368d3d
akshatgarg12 <akshatarungarg78@gmail.com>
2020-08-29 14:56:41 +0530

    Updates

---
+++ precache-manifest.efbb164654286a50615cda38b3c516e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "888fd5c797532fe72517ee09cd233538",
+    "url": "/GoldenRatio-Hack/index.html"
+  },
+  {
+    "revision": "aff0abb134537c694e15",
+    "url": "/GoldenRatio-Hack/static/css/main.d9cfa5e3.chunk.css"
+  },
+  {
+    "revision": "78021b44b03bad1b468e",
+    "url": "/GoldenRatio-Hack/static/js/2.bd878330.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GoldenRatio-Hack/static/js/2.bd878330.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aff0abb134537c694e15",
+    "url": "/GoldenRatio-Hack/static/js/main.c2568bf9.chunk.js"
+  },
+  {
+    "revision": "c2ff35f2c301c890ca21",
+    "url": "/GoldenRatio-Hack/static/js/runtime-main.d0e68cbb.js"
+  }
+]);
